58129d23b6300ff9deced30d023281cb7cf44481
Subhranil Das <subhranil@itobuz.com>
2025-02-14 00:41:03 +0530

    feat:delete user by admin

--- backend/src/controllers/adminController.js
+++ backend/src/controllers/adminController.js
@@ -1,5 +1,7 @@
+import notesModel from "../models/notesModel.js"
+import sessionsModel from "../models/sessionSchema.js"
 import userModel from "../models/userModel.js"
-import { INTERNAL_SERVER_ERROR_CODE, SUCCESS_CODE, UNAUTHORIZED_CODE } from "../utils/constant.js"
+import { INTERNAL_SERVER_ERROR_CODE, NOT_FOUND_CODE, SUCCESS_CODE, UNAUTHORIZED_CODE } from "../utils/constant.js"
 
 export const getAllUser = async (req, res) => {
     try {
@@ -16,6 +18,51 @@ export const getAllUser = async (req, res) => {
             message: 'All user fetched successfully',
             totalUser: users.length,
             data: users
+        })
+
+    } catch (error) {
+        return res.status(INTERNAL_SERVER_ERROR_CODE).json({
+            success: false,
+            message: error.message
+        })
+    }
+}
+
+
+export const deleteUser = async (req, res) => {
+    try {
+        if (req.role === 'user') {
+            return res.status(UNAUTHORIZED_CODE).json({
+                success: false,
+                message: "Unauthorized access"
+            })
+        }
+
+        const userId = req.params.userId
+
+        if (!userId) {
+            return res.status(NOT_FOUND_CODE).json({
+                success: false,
+                message: "User id not found"
+            })
+        }
+
+        const user = await userModel.findById(userId)
+
+        if (!user) {
+            return res.status(NOT_FOUND_CODE).json({
+                success: false,
+                message: "User not found"
+            })
+        }
+
+        await notesModel.deleteMany({ user: userId })
+        await sessionsModel.deleteMany({ userId: userId })
+        await userModel.deleteOne({ _id: userId })
+
+        return res.status(200).json({
+            success: true,
+            message: "User deleted successfully"
         })
 
     } catch (error) {
--- backend/src/routes/adminRoute.js
+++ backend/src/routes/adminRoute.js
@@ -1,11 +1,12 @@
 import express from 'express'
 import isAuthenticated from '../middleware/isAuthenticated.js'
-import { getAllUser } from '../controllers/adminController.js'
+import { deleteUser, getAllUser } from '../controllers/adminController.js'
 
 
 const adminRoute = express.Router()
 
 
 adminRoute.get('/getAllUser', isAuthenticated, getAllUser)
+adminRoute.delete('/delete/user/:userId', isAuthenticated, deleteUser)
 
 export default adminRoute
